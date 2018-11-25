2fffb6f385b8f6559add54f5b4180559ffe65dcf
jenswaeckerle <jens.waeckerle@uni-koeln.de>
2018-11-25 14:36:49 +0100

    fixed bugs

--- R/wer.R
+++ R/wer.R
@@ -1,6 +1,6 @@
 #' Calculating the Word Error Rate
 #'
-#' This function calculates the word error rate betwenn a hypothesis and a reference corpus.
+#' This function calculates the word error rate between a hypothesis and a reference corpus.
 #' @param r The reference quanteda corpus
 #' @param h The hypothesis quanteda corpus
 #' @keywords word error rate
@@ -9,13 +9,12 @@
 #' substitutions, deletions and insertions and the number of words in the
 #' reference and hypothesis corpora for each text in the corpus.
 #' @examples
-#' library(quanteda)
 #' hypothesis_data=data.frame(text="The meadoww very nice and the two sun shines bright",
 #' name="doc1",stringsAsFactors = F)
-#' hypothesis_corpus=corpus(hypothesis_data,docid_field = "name", text_field = "text")
+#' hypothesis_corpus=quanteda::corpus(hypothesis_data,docid_field = "name", text_field = "text")
 #' reference_data=data.frame(text="The meadow is very nice and the sun shines bright",
 #' name="doc1",stringsAsFactors = F)
-#' reference_corpus=corpus(reference_data,docid_field = "name", text_field = "text")
+#' reference_corpus=quanteda::corpus(reference_data,docid_field = "name", text_field = "text")
 #' wer(r=reference_corpus,h=hypothesis_corpus)
 
 wer<-function(r,h){
